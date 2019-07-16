59f3fc1a70cf2f4abce9413557a800eabe6d4023
DannyZeng <15625140174@163.com>
2019-07-16 23:48:03 +0800

    getAllEmployees test passed

--- src/main/java/com/tw/apistackbase/controller/EmployeeController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.tw.apistackbase.controller;
 
+import com.tw.apistackbase.entity.Employee;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -14,11 +17,17 @@ import java.util.logging.Logger;
 public class EmployeeController {
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
+    private List<Employee> employees = new ArrayList<>();
+
+    private void initData() {
+        employees.add(new Employee(1, "Tony", 22, "male", 10000));
+        employees.add(new Employee(2, "Jerry", 25, "male", 15000));
+    }
 
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    @GetMapping()
+    public ResponseEntity getAllEmployees() {
+        initData();
+        return ResponseEntity.ok().body(employees);
     }
 }
---
+++ src/test/java/com/tw/apistackbase/EmployeeTest.java
@@ -0,0 +1,39 @@
+package com.tw.apistackbase;
+
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class EmployeeTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void return_company_list_when_get_all_company() throws Exception {
+        String content = mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        JSONArray jsonArray = new JSONArray(content);
+        assertEquals(1, jsonArray.getJSONObject(0).getInt("id"));
+        assertEquals("Tony", jsonArray.getJSONObject(0).getString("name"));
+        assertEquals(22, jsonArray.getJSONObject(0).getInt("age"));
+
+        assertEquals(2, jsonArray.getJSONObject(1).getInt("id"));
+        assertEquals("Jerry", jsonArray.getJSONObject(1).getString("name"));
+        assertEquals(25, jsonArray.getJSONObject(1).getInt("age"));
+
+    }
+}
